d562ae592b2b1c28f1af4d8e1a677954b1b308fd
Camila Gonzalez <camilagonzadeel@gmail.com>
2025-05-08 15:23:31 -0600

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Counter from './components/Counter.vue'
+import Question from './components/Question.vue';
+
+
+function mostrarMensaje(msj){
+  alert(msj)
+
+}
+
+</script>
+
+<template>
+
+  <h1>Mi contador</h1>
+  <Counter :value="15" @send="mostrarMensaje" />
+
+  <Question></Question>
+
+ 
+
+</template>
